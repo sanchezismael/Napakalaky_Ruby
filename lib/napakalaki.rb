a23d3130a62f5ccf831a1e877ad0bbd45b4264b0
sanchezismael <sanchezismael@correo.ugr.es>
2015-11-02 16:33:49 +0100

    P2: Terminada la declaracion de la clase Napakalaki Ruby

--- lib/napakalaki.rb
+++ lib/napakalaki.rb
@@ -3,7 +3,53 @@
 # and open the template in the editor.
 
 class Napakalaki
+  include singleton
+
+  attr_reader :Napakalaki.instance, :CurrentPlayer, :CurrentMonster
+  attr_writer :Enemies
   def initialize
     
   end
+  
+  private
+  def initPlayer(names)
+    
+  end
+  
+  def nextPlayer()
+    
+  end
+  
+  def nextTurnAllowed()
+    
+  end
+  
+  def developCombat()
+    
+  end
+  
+  def discardVisibleTreasures(treasures)
+    
+  end
+  
+  def discardHiddenTreasures(treasures)
+    
+  end
+  
+  def makeTreasuresVisible(treasures)
+    
+  end
+  
+  def initGame(players)
+    
+  end
+  
+  def nectTurn()
+    
+  end
+  
+  def endOfGame(result)
+    
+  end
+  
 end

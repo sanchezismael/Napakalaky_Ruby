4fff793f2cc28ee43ca314e42d453714a57c1847
sanchezismael <sanchezismael@correo.ugr.es>
2015-12-03 19:00:35 +0100

    napakalaki game

--- lib/GameTester.rb
+++ lib/GameTester.rb
@@ -41,16 +41,16 @@ class GameTester
         if (command == Command::Combat) then
           combatResult = @game.developCombat()
           case combatResult
-            when NapakalakiGame::CombatResult::WINGAME then 
+            when Napakalaki::CombatResult::WINGAME then 
               puts "\n\n       " + currentPlayer.name
               puts "\n\n HAS GANADO LA PARTIDA"
               #break está implícito            
-            when NapakalakiGame::CombatResult::WIN then
+            when Napakalaki::CombatResult::WIN then
               puts "\n\n Ganaste el combate"
-            when NapakalakiGame::CombatResult::LOSE then
+            when Napakalaki::CombatResult::LOSE then
               puts "\n\n Has perdido el combate, te toca cumplir el mal rollo"
            end #case
-           if (combatResult != NapakalakiGame::CombatResult::WINGAME) then
+           if (combatResult != Napakalaki::CombatResult::WINGAME) then
             begin #Hasta que se avance de turno 
               puts "******* ******* ******* ******* ******* ******* *******"
               puts "\n\n Turno de: " + currentPlayer.to_s()

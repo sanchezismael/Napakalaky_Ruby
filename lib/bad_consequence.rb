e2191369e6ac9b166cfcc7e204e8ee213ff9157d
sanchezismael <sanchezismael@correo.ugr.es>
2016-01-05 12:42:41 +0100

    quitado comentarios badconsequence

--- lib/bad_consequence.rb
+++ lib/bad_consequence.rb
@@ -7,25 +7,6 @@ module Napakalaki
 class BadConsequence
   
   @@MAXTREASURES = 10
-  
-  def initialize 
-  end
-  
-#    def self.newLevelNumberOfTreasures(aText, someLevels, someVisibleTreasures, 
-#        someHiddenTreasures)
-#      new(aText, someLevels, someVisibleTreasures, someHiddenTreasures,Array.new,Array.new,
-#      false)      
-#    end
-#    
-#    def self.newLevelSpecificTreasures(aText,someLevels,someSpecificVisibleTreasures,
-#      someSpecificHiddenTreasures)
-#      new(aText,someLevels,0,0,someSpecificVisibleTreasures,someSpecificHiddenTreasures,
-#      false)
-#    end
-#    
-#    def self.newDeath(aText, death)
-#      new(aText,0,0,0,Array.new,Array.new,death)
-#    end
     
     def isEmpty()
       raise NotImplementedError.new
@@ -48,19 +29,5 @@ class BadConsequence
       @@MAXTREASURES
     end
     
-   
-    
-#    def to_s
-#      "Name: #{@text},Niveles perdidos: #{@levels},Tesoros visibles perdidos: #{@nVisibleTreasures},Tesoros ocultos perdidos: #{@nHiddenTreasures},Muerto: #{@death}, Tesoros especificos visibles: #{@specificVisibleTreasures},Tesoros especificos ocultos: #{@specificHiddenTreasures}"
-#   
-#    end
-    
-#    private_class_method :new
-    
-#    un_bad = BadConsequence.newLevelSpecificTreasures('Pierdes la armadura visible',0, [TreasureKind::ARMOR], [TreasureKind::ARMOR])
-#    puts un_bad.to_s
-#    
-#    puts "Un_Bad: #{un_bad.specificVisibleTreasures}"
-#    puts "Un_Bad: #{un_bad.specificHiddenTreasures}"
 end
 end
